3f4ae9b233786c87ee010bde3b404dd0d45440eb
Steve Langasek <steve.langasek@ubuntu.com>
2016-11-29 11:09:41 -0800

    Imported Debian version 1.29ubuntu4

--- script/invoke-rc.d
+++ script/invoke-rc.d
@@ -639,6 +639,9 @@ if test x${FORCE} != x || test ${RC} -eq 104 ; then
 	    fi
 	done
 	printerror initscript ${INITSCRIPTID}, action \"${saction}\" failed.
+	if [ -n "$is_systemd" ] && [ "$saction" = start -o "$saction" = restart ]; then
+	    systemctl status --no-pager "${UNIT}" || true
+	fi
 	exit ${RC}
     fi
     exit 102
